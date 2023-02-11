19fafdb437304d8e6c06e39bd54b23683e425a46
Raghava <106142347+KV-Raghava@users.noreply.github.com>
2023-02-11 17:58:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+let [hours,minutes,seconds] = [0,0,0];
+let time = document.getElementById('time');
+let start = document.getElementById('start');
+start.addEventListener('click',timerStart);
+let pause = document.getElementById('pause');
+pause.addEventListener('click',timerStop);
+let reset = document.getElementById('reset');
+reset.addEventListener('click',timerReset);
+let index=null;
+function timer(){
+    seconds++;
+    if(seconds == 60) {
+        seconds =0;
+        minutes += 1;
+        if(minutes == 60){
+            minutes = 0;
+            hours += 1;
+        }
+    }
+    time.innerHTML = hours +" hrs: "+minutes+" mins: "+seconds+"secs";
+}
+
+function timerStart(){
+    if(index != null){
+        clearInterval(index);
+    }
+    index = setInterval(timer,1000);
+}
+
+function timerStop(){
+    clearInterval(index);
+}
+
+function timerReset(){
+    clearInterval(index);
+    seconds =0;
+    minutes=0;
+    hours=0;
+    time.innerHTML = hours +" hrs: "+minutes+" mins: "+seconds+"secs";
+}
